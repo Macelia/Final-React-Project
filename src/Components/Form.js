c1b5860ba76aa15dda691ec2462ac617ca7d9c66
Macelia <massiliasaidoune06@gmail.com>
2024-04-27 18:49:17 +0100

    commit

---
+++ src/Components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Form.css'
+
+const form = () => {
+  return (
+ 
+      <form className='form'>
+        <input type='text' className='text-fild'/>
+        <input type='submit' className='submit-button' value='Ajouter' />
+
+
+      </form>
+   
+  )
+}
+
+export default form
---
+++ src/Components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TodoList.css'
+
+const TodoList = () => {
+  return (
+    <div>
+        <h1> Ma To Do List </h1>
+      
+    </div>
+  )
+}
+
+export default TodoList
